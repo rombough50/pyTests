03354e63e67d753e8fc6cc647ed15892b079b46d
Jeffrey Rombough <40567865+rombough50@users.noreply.github.com>
2019-04-23 20:55:25 -0400

    added python

--- Test1.py
+++ Test1.py
@@ -5,3 +5,11 @@
 # Ok this is the last test to see if stuff can be pushed up there
 # Ok one last time with CODE before VS2019
 # that didnt work
+
+
+import tensorflow as tf #we import tensorflow
+import numpy as np #we import numpy
+sess = tf.Session() #start a new Session Object
+x_data = np.array([[1.,2.,3.],[3.,2.,6.]]) # 2x3 matrix
+x = tf.convert_to_tensor(x_data, dtype=tf.float32)
+print (x)
